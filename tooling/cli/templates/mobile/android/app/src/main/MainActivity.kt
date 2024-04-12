a4a441cb96220bc707bc0ca77c78b56f79cc5b40
Catchawink <threeeasywords@gmail.com>
2024-04-12 04:25:34 -0400

    Updated files.

--- tooling/cli/templates/mobile/android/app/src/main/MainActivity.kt
+++ tooling/cli/templates/mobile/android/app/src/main/MainActivity.kt
@@ -1,10 +1,10 @@
 package {{reverse-domain app.domain}}.{{snake-case app.name}}
 
 class MainActivity : TauriActivity() {
-  override fun onStart() {
-    super.onStart()
+  override fun onCreate() {
     if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
       android.webkit.WebView.setWebContentsDebuggingEnabled(true);
     }
+    super.onCreate()
   }
 }
